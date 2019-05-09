d95adf991022065ed608938769491efca21dec42
Tigran Hovhannisyan <htigran97@gmail.com>
2019-05-09 22:44:58 +0400

    2 Tabs. Kotayq map working.

--- server.R
+++ server.R
@@ -2,7 +2,22 @@
 
 server <- function(input, output, session) {
   
+  ## Kotayk tab
+  kotaykData <- readOGR("Kotayk_Province", "kotayk")
+  kotaykData <- spTransform(kotaykData, CRS("+proj=longlat +datum=WGS84 +no_defs"))
   
+  output$kotaykMap <- renderLeaflet({
+    # Use leaflet() here, and only include aspects of the map that
+    # won't need to change dynamically (at least, not unless the
+    # entire map is being torn down and recreated).
+    leaflet(kotaykData) %>% addTiles() %>% 
+      addPolygons(weight = 1, smoothFactor = 0.05,
+                  opacity = 1.0, fillOpacity = 0.05,, 
+                  highlightOptions = highlightOptions(color = "white", weight = 2, bringToFront = TRUE))
+  })
+  
+  
+  ## Armenia
   points <- eventReactive(input$recalc, {
     cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
   }, ignoreNULL = FALSE)
@@ -14,15 +29,19 @@ server <- function(input, output, session) {
   
   output$Map <- renderLeaflet({
     leaflet(shapeData) %>%
-      addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+      addPolygons(color = "#444444", weight = 1, smoothFactor = 0.05,
                   opacity = 1.0, fillOpacity = 0.5, layerId = shapeData$MarzID,
                   #fillColor = ~colorQuantile("YlOrRd", ALAND)(ALAND),
                   highlightOptions = highlightOptions(color = "white", weight = 2,
                                                       bringToFront = TRUE))
   })
   
+  
   observeEvent(input$Map_shape_click, { # update the location selectInput on map clicks
     p <- input$Map_shape_click
-    print(p$id) ## If of the marz
+    print(p$id) ## Id of the marz
+    tmp <- c(tmp, p$id)
+    print(tmp)
   })
+  
 }
--- ui.R
+++ ui.R
@@ -1,24 +1,36 @@
 
 ui <- fluidPage(
   
-  # App title ----
-  titlePanel("Cadastre!"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      leafletOutput("Map")
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      
-    )
-  )
-  
+  navbarPage( title = "Cadastre",
+              
+              tabPanel( "Monuments",
+                        sidebarPanel(
+                          leafletOutput("kotaykMap")
+                        )
+                        
+                        
+                        
+              ), # tablPanel - Kotayk
+              
+              
+              tabPanel( "Real Estate",
+                        
+                        # Sidebar layout with input and output definitions ----
+                        sidebarLayout(
+                          
+                          # Sidebar panel for inputs ----
+                          sidebarPanel(
+                            leafletOutput("Map")
+                          ),
+                          
+                          # Main panel for displaying outputs ----
+                          mainPanel(
+                            
+                            textOutput("selected_ids")
+                            
+                          ) # mainpanel
+                        )
+              ) # tablPanel - Armenia
+              
+  ) # navbarPage
 )
