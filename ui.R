9dfc07f0d6281402477ecd9cc1201d53fe3aa360
Henry Harutyunyan <henrik.harutyunyan@vxsoft.com>
2019-05-13 01:34:31 +0400

    UI modifications.

--- ui.R
+++ ui.R
@@ -1,32 +1,19 @@
 
-ui <- fluidPage(
-  navbarPage( title = "Cadastre",
-    tabPanel( "Landmarks",
-      div(class="outer",
-        tags$head(
-          includeCSS("styles.css"),
-          includeScript("gomap.js")
-        ),
-        leafletOutput("kotaykMap", width="100%", height="100%"),
-        absolutePanel(id = "controls", class = "panel panel-default", 
-            fixed = TRUE, draggable = TRUE, top = 60, left = "auto", 
-            right = 20, bottom = "auto", width = 330, height = "auto",
-          h2("Landmark explorer"),
-          htmlOutput("select_monument_type_UI"),
-          plotOutput("kotayk_piechart_by_type", height = 250)
-        )
-      )                             
-    ),          
-
-    tabPanel( "Real Estate",
-      sidebarLayout(
-        sidebarPanel(
-          leafletOutput("Map")
-        ),
-        mainPanel(
-          textOutput("selected_ids")
-        )
+ui <- fluidPage(title = "Cadastre Landmark Explorer",
+  tabPanel( "Landmarks",
+    div(class="outer",
+      tags$head(
+        includeCSS("styles.css"),
+        includeScript("gomap.js")
+      ),
+      leafletOutput("kotaykMap", width="100%", height="100%"),
+      absolutePanel(id = "controls", class = "panel panel-default", 
+          fixed = TRUE, draggable = TRUE, top = 20, left = "auto", 
+          right = 20, bottom = "auto", width = 330, height = "auto",
+        h2("Landmark explorer"),
+        htmlOutput("select_monument_type_UI"),
+        plotOutput("kotayk_piechart_by_type", height = 250)
       )
-    )           
+    )  
   )
 )
